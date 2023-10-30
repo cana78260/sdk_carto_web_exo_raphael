c888bc5a2f21032885967420268fea0094e65049
eric <eric.jalfre@sncf.fr>
2023-10-30 16:20:56 +0100

    affichages markers ok + bouton disable ko

--- src/Pages/Map.tsx
+++ src/Pages/Map.tsx
@@ -1,5 +1,5 @@
 import React, { useRef, useEffect, useState } from "react";
-import maplibregl from "maplibre-gl";
+import maplibregl, { Marker } from "maplibre-gl";
 import mapboxgl from "mapbox-gl";
 import "maplibre-gl/dist/maplibre-gl.css";
 import "./Map.css";
@@ -19,6 +19,48 @@ export default function Map() {
 
   const API_KEY = "UdqcpQrjVZzZzvNptXPW";
 
+  const disabledMarkers = document.querySelector(".disableMarker");
+
+const geoJson = 
+{
+      type: "FeatureCollection",
+      features: [
+        {
+       
+          type: "Feature",
+          properties: {
+            title: "OSAKA",
+          },
+          geometry: {
+            type: "Point",
+            coordinates: [135.502, 34.693],
+          },
+        },
+        {
+          type: "Feature",
+          properties: {
+            title: "SUITA",
+          },
+          geometry: {
+            type: "Point",
+            coordinates: [135.529, 34.7614],
+          },
+        },
+        {
+          type: "Feature",
+          properties: {
+            title: "AMAGASAKI",
+          },
+          geometry: {
+            type: "Point",
+            coordinates: [135.4095, 34.7069],
+          },
+        },
+      ],
+    }
+
+
+  
 
 
   useEffect(() => {
@@ -34,74 +76,74 @@ export default function Map() {
 // var featureLayer = L.mapbox.featureLayer(geojson).addTo(map);
 // featureLayer.loadUrl()
 map.on('load', ()=>{
-  map.addSource('japan', {
-    'type': 'geojson',
-    'data': {
-      'type': 'FeatureCollection',
-      'features': [
-        {
-          'type': 'Feature',
-          'properties': {},
-          'geometry': {
-            'type': 'Polygon',
-            'coordinates': [
-              [
-                [135.3460693359375, 34.709726932134586],
-                [135.4844284057617, 34.59110646239981],
-                [135.54725646972656, 34.79970851207568],
-                [135.41301727294922, 34.77151167170742],
-                [135.3460693359375, 34.709726932134586],
-              ],
-            ],
-          },
-        },
-        {
-          'type': 'Feature',
-          // 'properties': {
-          //   'title': 'OSAKA',
-          // },
-          'geometry': { 
-            'type': 'Point',
-            'coordinates': [135.502, 34.693]
-          
-          },
-        },
-        {
-          'type': 'Feature',
-          // 'properties': {
-          //   'title': 'YODOGAWA',
-          // },
-          'geometry': {
-            'type': 'Point',
-            'coordinates': [135.291, 34.435],
-          },
-        },
-        {
-          'type': 'Feature',
-          // 'properties': {
-          //   'title': 'AMAGASAKI',
-          // },
-          'geometry': {
-            'type': 'point',
-            'coordinates': [135.25, 34.43],
-          },
-        },
-      ],
-    },
+  map.addSource("japan", {
+    type: "geojson",
+    data: geoJson
+    // {
+    //   type: "FeatureCollection",
+    //   features: [
+    //     {
+    //     //   type: "Feature",
+    //     //   properties: {},
+    //     //   geometry: {
+    //     //     type: "Polygon",
+    //     //     coordinates: [
+    //     //       [
+    //     //         [135.3460693359375, 34.709726932134586],
+    //     //         [135.4844284057617, 34.59110646239981],
+    //     //         [135.54725646972656, 34.79970851207568],
+    //     //         [135.41301727294922, 34.77151167170742],
+    //     //         [135.3460693359375, 34.709726932134586],
+    //     //       ],
+    //     //     ],
+    //     //   },
+    //     // },
+    //     // {
+    //       type: "Feature",
+    //       properties: {
+    //         title: "OSAKA",
+    //       },
+    //       geometry: {
+    //         type: "Point",
+    //         coordinates: [135.502, 34.693],
+    //       },
+    //     },
+    //     {
+    //       type: "Feature",
+    //       properties: {
+    //         title: "SUITA",
+    //       },
+    //       geometry: {
+    //         type: "Point",
+    //         coordinates: [135.529, 34.7614],
+    //       },
+    //     },
+    //     {
+    //       type: "Feature",
+    //       properties: {
+    //         title: "AMAGASAKI",
+    //       },
+    //       geometry: {
+    //         type: "Point",
+    //         coordinates: [135.4095, 34.7069],
+    //       },
+    //     },
+    //   ],
+    // },
   });
-  map.addLayer({
-    'id': 'id',
-    'type': 'fill',
-    'source': 'japan',
-    'paint': {
-      "fill-color": '#888888',
-      "fill-opacity": 0.4,
-    },
-    'filter': ['==', '$type', 'Polygon'],
-  });
+  // map.addLayer({
+  //   'id': 'polygone',
+  //   'type': 'fill',
+  //   'source': 'japan',
+  //   'paint': {
+  //     "fill-color": '#888888',
+  //     "fill-opacity": 0.4,
+  //   },
+  //   'filter': ['==', '$type', 'Polygon'],
+  // });
 
   map.addLayer({
-    'id': 'id-point',
+    'id': 'points',
     'type':'circle',
     'source':'japan',
     'paint': {
@@ -111,17 +153,36 @@ map.on('load', ()=>{
     'filter': ['==','$type','Point']
   });
 })
+console.log(geoJson.features)
+ const tab: Marker[] = [];
+  geoJson.features.forEach((feature) => {
+    if (feature.geometry.type === "Point" && feature.geometry.coordinates) {
+      const coordinates = feature.geometry.coordinates;
+     
+       tab.push(new maplibregl.Marker({ color: "#FF0000" })
+         .setLngLat([coordinates[0],coordinates[1]])
+         .addTo(map));
 
-
+    }
+     disabledMarkers?.addEventListener('click',()=> {
+      // const coordinates = feature.geometry.coordinates;
+       tab.forEach((marker)=>{
+        console.log(marker.getLngLat())
+        marker.remove();
+       })
+    })
+  });
   map.on("moveend", () => {
     let currentMapZoom = map.getZoom();
     setZoomLevel(Number(currentMapZoom.toFixed(1)));
   });
 
     map.addControl(new maplibregl.NavigationControl(), "top-right");
-    new maplibregl.Marker({ color: "#FF0000" })
-      .setLngLat([lng, lat])
-      .addTo(map);
+    // new maplibregl.Marker({ color: "#FF0000" })
+    //   .setLngLat([lng, lat])
+    //   .addTo(map);
+  
+    
   }, []);
 
   return (
